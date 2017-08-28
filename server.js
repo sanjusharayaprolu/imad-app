a735273147e5ed5608fe5ec3ac92c3a7738fb4e7
sanjusharayaprolu <30657424+sanjusharayaprolu@users.noreply.github.com>
2017-08-28 22:51:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -70,12 +70,14 @@ app.get('/counter',function(req,res){
     counter=counter + 1;
     res.send(counter.toString());
 });
+
 var names=[];
 app.get('/submit-name/:name',function(req,res){
     var name=req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
 });
+
 app.get('/:articlename',function(req,res){
     //articlename_article-one;
     //articles[articlename]=={}content object for article one

02b049958587c81d8f14e6661698cd96807f6b64
sanjusharayaprolu <30657424+sanjusharayaprolu@users.noreply.github.com>
2017-08-28 21:59:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,19 @@ var button=document.getElemntById('counter');
 var counter=0;
 button.onclick=function()
 {
-  counter=counter + 1;
-  var span=document.getElementById('count');
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function()
+    {
+        if(request.readyState===XMLHttpRequest.DONE)
+        {
+            if(request.status===200)
+            {
+               var counter= request.responseText;
+               var span=document.getElementById('count');
   span.innerHTML=counter.toString();
+            }
+        }
+    };
+  request.open('GET','http://sanjushar99.imad.hasura-app.io/counter',true);
+  request.send(null);
 };

8ba8628d2a84b868ef62384687938e9a0eccf0b6
sanjusharayaprolu <30657424+sanjusharayaprolu@users.noreply.github.com>
2017-08-28 18:11:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element;
 element.innerHTML='new value';
+
+var img=document.getElementById('img');
+img.onClicj=function()
+{
+    img.style.marginLeft='100px';
+};

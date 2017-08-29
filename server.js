7031fe161c42acb2a857f0b8e48e00036ebd2135
sanjusharayaprolu <30657424+sanjusharayaprolu@users.noreply.github.com>
2017-08-29 15:29:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,7 +71,12 @@ app.get('/counter',function(req,res){
     res.send(counter.toString());
 });
 
-
+var names=[];
+app.get('submit-name/:name',function(req,res){
+    var name=req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
 
 app.get('/:articlename',function(req,res){
     //articlename_article-one;
@@ -91,12 +96,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var names=[];
-app.get('/submit-name/:name',function(req,res){
-    var name=req.query.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
-});
+
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 

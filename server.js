9642899768d3662d3fb0cbd1d50da41524ffe7b3
sanjusharayaprolu <30657424+sanjusharayaprolu@users.noreply.github.com>
2017-08-31 19:12:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.get('/', function (req, res) {
 function hash(input,salt)
 {
     var hashed=crypto.pbkdf2Sync(input,salt,1000,512,'sha512');
-    return hashed.toSting('hex');
+    return hashed.toString('hex');
 }
 app.get('/hash/:input',function(req,res)
 {

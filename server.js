0a7de8b5d3b1c2a472833f00e9410f68ba56c9d7
sanjusharayaprolu <30657424+sanjusharayaprolu@users.noreply.github.com>
2017-08-31 19:11:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
+var crypto=require('crypto');
 var config={
     user:'sanjushar99',
     database:'sanjushar99',
@@ -49,6 +50,16 @@ return htmltemplate;
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+function hash(input)
+{
+    var hashed=crypto.pbkdf2Sync(input,salt,1000,512,'sha512');
+    return hashed.toSting('hex');
+}
+app.get('/hash/:input',function(req,res)
+{
+    var hashedString=hash(req.params.input,'this-is-some-random-string');
+    res.send(hashedString);
 });
 var pool= new Pool(config);
 app.get('/test-db',function(req,res)
